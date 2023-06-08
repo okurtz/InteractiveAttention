dbccf1331176c582a7ab2b0d015faf72fb98d436
unknown <Oliver.Kurtz@studium.fernuni-hagen.de>
2023-06-09 00:53:24 +0200

    fix transition matrix creator

--- transition_matrix_creator.jl
+++ transition_matrix_creator.jl
@@ -1,4 +1,8 @@
 module transition_matrix_creator
+    export
+        get_transition_matrix,
+        get_starting_point_probabilities
+
     using 
         CSV,
         DataFrames,
@@ -8,14 +12,14 @@ module transition_matrix_creator
     cd("C:\\Users\\Oliver\\Documents\\Studium\\Psychologie\\Bachelorarbeit\\Skripte");
     const GAMBLE_SOURCE_PATH = "data/Gambles_Fiedler_Glöckner_2012_standardized.csv";
     const GAMBLES = CSV.read(GAMBLE_SOURCE_PATH, DataFrame);
-    const TARGETS = ["1.0", "2.0", "3.0", "4.0", "5.0", "6.0", "7.0", "8.0"];     # Do not change the order!
+    const TARGETS = ["Ap1", "Ap2", "Bp1", "Bp2", "Av1", "Av2", "Bv1", "Bv2"];     # Assuming that array index corresponds to AOI number
     const B_OPTN = hcat(repeat([1 1 0 0 1 1 0 0], 8));
     const B_ATTR = hcat(repeat([0 0 0 0 1 1 1 1], 8));
     const B_BRAN = hcat(repeat([1 0 1 0 1 0 1 0], 8));  # Assuming top-most option = 1st option, bottom-most option = 2nd option
     const I_OPTN = repeat(vcat(repeat([1 1 0 0 1 1 0 0], 2), 
                             repeat([0 0 1 1 0 0 1 1], 2)), 2);
     const I_ATTR = [j < 4 ? (i >= 4 ? 0 : 1) : (i >= 4 ? 1 : 0)  for i in 0:7, j in 0:7];
-    const I_STAT = I;
+    const I_STAT = Matrix{Int}(I, 8, 8);
     const I_P_BRAN = [i >= 4 ? 0 : i === j || (i+4)%8 === j ? 1 : 0 for i in 0:7, j in 0:7];
     const I_X_BRAN = [i < 4 ? 0 : i === j || (i+4)%8 === j ? 1 : 0 for i in 0:7, j in 0:7];
 
@@ -53,25 +57,21 @@ module transition_matrix_creator
         y = maximum(v);
         return y + log(sum(exp.(v .- y)));
     end
+
     function softmax(x)
         return exp.(x .- logsumexp(x));
     end
 
-    # Übergangsmatrizen der Teilnehmer für alle Gambles bestimmen
-    # Für alle Teilnehmer die Beta-Parameter einlesen
-    # Mit diesen Beta-Parameter die Modellimplementation aufrufen und die jeweilige Übergangsmatrix berechnen lassen
-
-    # Diese Funktion wendet das Modell an, um die Übergangsmatrizen zu berechnen.
     """ Calculates the probabilities to transition to any state given the current state.
     This implementation does not consider a novelty parameter.
     # Arguments
-    - currentTarget: Vurrent target as specified in TARGETS
+    - currentTarget: Current target as specified in TARGETS
     - gamble: Number of the gamble for which to calculate transition probabilities, referring to gambles.trigger
     - betas: beta1-10 as specified by He and Bhatia (2023)
     # Returns
     - A vector containing the transitioning probabilities with the array indices referring to the target indices
     """
-    function get_transition_probabilities(currentTarget::String, gamble::Number, betas::Array{Number})
+    function get_transition_probabilities(currentTarget::String, gamble::Number, betas)
         z = GAMBLES[gamble, currentTarget*"_z"];
         i = findall(x -> x === currentTarget, TARGETS);
 
@@ -89,7 +89,11 @@ module transition_matrix_creator
         );
     end
 
-    global function get_transition_matrix(gamble::Number, betas::Array{Number})
-        return [get_transition_probabilities(TARGETS[i], gamble, betas) for i in 1:8];
+    function get_transition_matrix(gamble::Number, betas)
+        transitionMatrix = Matrix{Float64}(undef,8,8);
+        for i in 1:8
+            transitionMatrix[i,:] = get_transition_probabilities(TARGETS[i], gamble, betas);
+        end
+        return transitionMatrix;
     end
 end
