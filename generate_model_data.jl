17a61344790feb8161f4e814dc768a67540e54c7
unknown <Oliver.Kurtz@studium.fernuni-hagen.de>
2023-07-08 22:57:29 +0200

    add functional simulation and processing for hypothesis 3

--- generate_model_data.jl
+++ generate_model_data.jl
@@ -14,34 +14,43 @@ include("generation_util.jl");
 println("Startup completed. Loading data...")
 
 const PREDICTIONS_PER_GAMBLE::Int64 = 1;
+const MISSING_GAMBLES::Int64 = 4;   # Gambles 4, 35 and 45 of subject 4, gamble 27 of subject 17.
 const SOURCE_FILE_NAME::String = "data/Study 1/Fiedler_Glöckner_2012_Exp1_transition_matrices";
-const OUTPUT_FILE_NAME::String = "data/Study 1/model_forecasts_per_participant.csv";
+const HYPOTHESIS_3_OUTPUT_FILE_NAME::String = "data/Study 1/hypothesis_3_aggregated_data.csv";
 const GAMBLES::DataFrame = @pipe CSV.read("data/Study 1/Observations_Fiedler_Glöckner_2012_study_1_preprocessed.csv", DataFrame, types=Dict([(:subject, Int64), (:trigger, Int64)]), silencewarnings=false) |>
     _[!, [:subject, :trigger, :AOI]] |> groupby(_, [:subject, :trigger]) |> combine(_, nrow => :numberOfSamples, renamecols=false);
 const BETAS::DataFrame = CSV.read("data/Study 1/parameters_10_FiedlerGloeckner2012_EXP1.csv", DataFrame);
+const NUM_SUBJECTS = length(unique(GAMBLES.subject));
+const NUM_GAMBLES = length(unique(GAMBLES.trigger));
 
-allSamplingPaths::DataFrame = DataFrame(generation_util.newSamplingPath(0));
+hypothesis3Data::NamedTuple{(:subject, :gamble, :Av1, :Av2, :Bv1, :Bv2), Tuple{Vector{Int64}, Vector{Int64}, Vector{Float64}, Vector{Float64}, Vector{Float64}, Vector{Float64}}} = generation_util.newHypothesis3Data(NUM_SUBJECTS * NUM_GAMBLES - MISSING_GAMBLES);
 @load SOURCE_FILE_NAME*".jld2" transitionMatrices;  # first index: subject, second index: gamble
+
+l = Threads.SpinLock();
 
 println(@sprintf("Starting model data computation using %i threads.", Threads.nthreads()));
 
 Threads.@threads for gamble::DataFrameRow{DataFrame, DataFrames.Index} in eachrow(GAMBLES)
     currentBetas::DataFrameRow{DataFrame, DataFrames.SubIndex{DataFrames.Index, Vector{Int64}, Vector{Int64}}} = BETAS[BETAS[!, :subject] .== gamble.subject,:][1, Not(:subject)];
     currentMatrix::NamedMatrix{Float64, Matrix{Float64}, Tuple{OrderedDict{String, Int64}, OrderedDict{String, Int64}}} = transitionMatrices[gamble.subject, gamble.trigger];
+    currentSamplingPaths::NamedTuple{(:subject, :gamble, :path, :sample, :AOI), Tuple{Vector{Int64}, Vector{Int64}, Vector{Int64}, Vector{Int64}, Vector{String}}} = generation_util.simulate(currentMatrix, currentBetas, gamble, PREDICTIONS_PER_GAMBLE);
+    currentHypothesis3Data::NamedTuple{(:subject, :gamble, :Av1, :Av2, :Bv1, :Bv2), Tuple{Int64, Int64, Float64, Float64, Float64, Float64}} = generation_util.calculateHypothesis3(currentSamplingPaths);
 
-    currentSamplingPaths = generation_util.simulate(currentMatrix, currentBetas, gamble, PREDICTIONS_PER_GAMBLE);
-    lock(allSamplingPaths) do
-        append!(allSamplingPaths, DataFrame(currentSamplingPaths));
-    unlock(allSamplingPaths);
+    # Threads.lock(l)
+        map(x -> (global hypothesis3Data[x][rownumber(gamble)] = currentHypothesis3Data[x]), keys(currentHypothesis3Data));
+    # Threads.unlock(l)
     # Hier liegen alle Sampling-Pfade des gegenwärtigen Teilnehmers für das gegenwärtige Spiel vor.
 
     # Hypothese 3: Durchschnittliche Anzahl der Fixationen pro AOI
     # Hypothese 5: Anteil letztlich gewählten Option in den ersten zwei Dritteln und im letzten Drittel
     # Hypothese 6: Anteil der Samples auf ein Wahrscheinlichkeitsattribut im ersten Fünftel und in den übrigen vier Fünfteln
     # Hypothese 7: Durchschnittliche Anzahl der Fixationsübergänge innerhalb einer Option u. zwischen den Optionen
+
+    # Ergebnis: Vier Ausgabedateien, eine für jede Hypothese.
     println(@sprintf("Simulated %i samples in %i iterations of gamble %i, participant %i.", gamble.numberOfSamples, PREDICTIONS_PER_GAMBLE, gamble.trigger, gamble.subject))
 end
 
-CSV.write(OUTPUT_FILE_NAME, allSamplingPaths);
+rm(HYPOTHESIS_3_OUTPUT_FILE_NAME, force=true);
+CSV.write(HYPOTHESIS_3_OUTPUT_FILE_NAME, hypothesis3Data);
 
 println("Run finished.")
--- generation_util.jl
+++ generation_util.jl
@@ -15,13 +15,13 @@ module generation_util
                 AOI = Array{String}(undef, initialSize));
     end
 
-    insert = function(samplingPath::NamedTuple{(:subject, :gamble, :path, :sample, :AOI), Tuple{Vector{Int64}, Vector{Int64}, Vector{Int64}, Vector{Int64}, Vector{String}}},
-                      subject::Int64, gamble::Int64, path::Int64, sample::Int64, AOI::String)
-        samplingPath.subject[sample] = subject;
-        samplingPath.gamble[sample] = gamble;
-        samplingPath.path[sample] = path;
-        samplingPath.sample[sample] = sample;
-        samplingPath.AOI[sample] = AOI;
+    newHypothesis3Data = function(initialSize::Int64)
+        return (subject = Array{Int64}(undef, initialSize),     # participant
+                gamble = Array{Int64}(undef, initialSize),      # trigger
+                Av1 = Array{Float64}(undef, initialSize),
+                Av2 = Array{Float64}(undef, initialSize),
+                Bv1 = Array{Float64}(undef, initialSize),
+                Bv2 = Array{Float64}(undef, initialSize));
     end
 
     simulate = function(transitionMatrix::NamedMatrix{Float64, Matrix{Float64}, Tuple{OrderedDict{String, Int64}, OrderedDict{String, Int64}}},
@@ -34,14 +34,32 @@ module generation_util
 
         for i in 0:iterations-1
             currentState = StatsBase.sample(transition_matrix_creator.TARGETS, Weights(transition_matrix_creator.get_starting_point_probabilities(betas[1], betas[2], betas[3])), 1)[1];
-            generation_util.insert(samplingPaths, gamble.subject, gamble.trigger, i+1, 1 + i * gamble.numberOfSamples, currentState);
+            map(x -> samplingPaths[x][1+i*gamble.numberOfSamples] = (gamble.subject, gamble.trigger, i+1, 1+i*gamble.numberOfSamples, currentState)[x], 1:5);
     
             for sample in 2:gamble.numberOfSamples
                 currentState = StatsBase.sample(transition_matrix_creator.TARGETS, Weights(vec(transitionMatrix[currentState,:])), 1)[1];
-                generation_util.insert(samplingPaths, gamble.subject, gamble.trigger, i+1, sample + i * gamble.numberOfSamples, currentState);
+                map(x -> samplingPaths[x][sample+i*gamble.numberOfSamples] = (gamble.subject, gamble.trigger, i+1, sample+i*gamble.numberOfSamples, currentState)[x], 1:5);
             end
         end
 
         return samplingPaths;
+    end
+
+    """ Hypothesis 3: The amount of fixations spent on an outcome increases with both is probability and the value of the outcome.
+    # Arguments
+    - samplingPath: An array of sampling sequences of a single participant in a single gamble
+    # Returns
+    - A NamedTuple containing the average percentage of fixations on an outcome across all sampling sequences
+    """
+    calculateHypothesis3 = function(samplingPath::NamedTuple{(:subject, :gamble, :path, :sample, :AOI), Tuple{Vector{Int64}, Vector{Int64}, Vector{Int64}, Vector{Int64}, Vector{String}}})
+        numAOIs = countmap(samplingPath[:AOI]);
+        numTotalSamples = length(samplingPath[1]);
+
+        return (subject = samplingPath.subject[1],
+                gamble = samplingPath.gamble[1],
+                Av1 = (haskey(numAOIs, "Av1") ? numAOIs["Av1"] : 0) / numTotalSamples,
+                Av2 = (haskey(numAOIs, "Av2") ? numAOIs["Av2"] : 0) / numTotalSamples,
+                Bv1 = (haskey(numAOIs, "Bv1") ? numAOIs["Bv1"] : 0) / numTotalSamples,
+                Bv2 = (haskey(numAOIs, "Bv2") ? numAOIs["Bv2"] : 0) / numTotalSamples);
     end
 end
