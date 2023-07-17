435cc3880504ed6f5733170cf3bb53d20e95be74
unknown <Oliver.Kurtz@studium.fernuni-hagen.de>
2023-07-17 13:10:34 +0200

    add data processing for hypothesis 5

--- generate_model_data.jl
+++ generate_model_data.jl
@@ -1,3 +1,4 @@
+println("Starting up...")
 using
     CSV,
     DataFrames,
@@ -12,32 +13,32 @@ include("generation_util.jl");
 
 println("Startup completed. Loading data...")
 
-const PREDICTIONS_PER_GAMBLE::Int64 = 1;
+const STUDY = 1;
+const PREDICTIONS_PER_GAMBLE::Int64 = 5;
 const MISSING_GAMBLES::Int64 = 4;   # Gambles 4, 35 and 45 of subject 4, gamble 27 of subject 17.
 const SOURCE_FILE_NAME::String = "data/Study 1/Fiedler_Glöckner_2012_Exp1_transition_matrices";
-const HYPOTHESIS_3_OUTPUT_FILE_NAME::String = "data/Study 1/hypothesis_3_aggregated_data.csv";
 const GAMBLES::DataFrame = CSV.read("data/Study 1/Observations_Fiedler_Glöckner_2012_study_1_aggregated.csv", DataFrame, types=Dict([(:optionChosen, Char)]));
 const BETAS::DataFrame = CSV.read("data/Study 1/parameters_10_FiedlerGloeckner2012_EXP1.csv", DataFrame);
-const NUM_SUBJECTS = length(unique(GAMBLES.subject));
-const NUM_GAMBLES = length(unique(GAMBLES.trigger));
+const NUM_SUBJECTS::Int64 = length(unique(GAMBLES.subject));
+const NUM_GAMBLES::Int64 = length(unique(GAMBLES.trigger));
+const INITIAL_STRUCTURE_SIZE::Int64 = NUM_SUBJECTS * NUM_GAMBLES - MISSING_GAMBLES;
 
-hypothesis3Data::NamedTuple{(:subject, :gamble, :Av1, :Av2, :Bv1, :Bv2), Tuple{Vector{Int64}, Vector{Int64}, Vector{Float64}, Vector{Float64}, Vector{Float64}, Vector{Float64}}} = generation_util.newHypothesis3Data(NUM_SUBJECTS * NUM_GAMBLES - MISSING_GAMBLES);
+hypothesis3Data::NamedTuple{(:subject, :gamble, :Av1, :Av2, :Bv1, :Bv2), Tuple{Vector{Int64}, Vector{Int64}, Vector{Float64}, Vector{Float64}, Vector{Float64}, Vector{Float64}}} = generation_util.newHypothesis3Data(INITIAL_STRUCTURE_SIZE);
+hypothesis5Data::NamedTuple{(:subject, :gamble, :optionChosen, Symbol("10%"), Symbol("20%"), Symbol("30%"), Symbol("40%"), Symbol("50%"), Symbol("60%"), Symbol("70%"), Symbol("80%"), Symbol("90%"), Symbol("100%")), Tuple{Vector{Int64}, Vector{Int64}, Vector{Char}, Vector{Union{Missing, Float64}}, Vector{Union{Missing, Float64}}, Vector{Union{Missing, Float64}}, Vector{Union{Missing, Float64}}, Vector{Union{Missing, Float64}}, Vector{Union{Missing, Float64}}, Vector{Union{Missing, Float64}}, Vector{Union{Missing, Float64}}, Vector{Union{Missing, Float64}}, Vector{Union{Missing, Float64}}}} = generation_util.newHypothesis5Data(INITIAL_STRUCTURE_SIZE);
 @load SOURCE_FILE_NAME*".jld2" transitionMatrices;  # first index: subject, second index: gamble
-
-l = Threads.SpinLock();
 
 println(@sprintf("Starting model data computation using %i threads.", Threads.nthreads()));
 
 Threads.@threads for gamble::DataFrameRow{DataFrame, DataFrames.Index} in eachrow(GAMBLES)
     currentBetas::DataFrameRow{DataFrame, DataFrames.SubIndex{DataFrames.Index, Vector{Int64}, Vector{Int64}}} = BETAS[BETAS[!, :subject] .== gamble.subject,:][1, Not(:subject)];
     currentMatrix::NamedMatrix{Float64, Matrix{Float64}, Tuple{OrderedDict{String, Int64}, OrderedDict{String, Int64}}} = transitionMatrices[gamble.subject, gamble.trigger];
-    currentSamplingPaths::NamedTuple{(:subject, :gamble, :path, :sample, :AOI), Tuple{Vector{Int64}, Vector{Int64}, Vector{Int64}, Vector{Int64}, Vector{String}}} = generation_util.simulate(currentMatrix, currentBetas, gamble, PREDICTIONS_PER_GAMBLE);
-    currentHypothesis3Data::NamedTuple{(:subject, :gamble, :Av1, :Av2, :Bv1, :Bv2), Tuple{Int64, Int64, Float64, Float64, Float64, Float64}} = generation_util.calculateHypothesis3(currentSamplingPaths);
+    currentSamplingPaths::DataFrame = generation_util.simulate(currentMatrix, currentBetas, gamble, PREDICTIONS_PER_GAMBLE);
 
-    # Threads.lock(l)
-    map(x -> (global hypothesis3Data[x][rownumber(gamble)] = currentHypothesis3Data[x]), keys(currentHypothesis3Data));
-    # Threads.unlock(l)
-    # Hier liegen alle Sampling-Pfade des gegenwärtigen Teilnehmers für das gegenwärtige Spiel vor.
+    currentHypothesis3Data::DataFrameRow{DataFrame, DataFrames.Index} = generation_util.calculateHypothesis3(currentSamplingPaths);
+    currentHypothesis5Data::DataFrameRow{DataFrame, DataFrames.Index} = generation_util.calculateHypothesis5(currentSamplingPaths, gamble.optionChosen);
+
+    foreach(x -> (global hypothesis3Data[x][rownumber(gamble)] = currentHypothesis3Data[x]), keys(currentHypothesis3Data));
+    foreach(x -> (global hypothesis5Data[x][rownumber(gamble)] = currentHypothesis5Data[x]), keys(currentHypothesis5Data));
 
     # Hypothese 3: Durchschnittliche Anzahl der Fixationen pro AOI
     # Hypothese 5: Anteil letztlich gewählten Option in den ersten zwei Dritteln und im letzten Drittel
@@ -45,10 +46,13 @@ Threads.@threads for gamble::DataFrameRow{DataFrame, DataFrames.Index} in eachro
     # Hypothese 7: Durchschnittliche Anzahl der Fixationsübergänge innerhalb einer Option u. zwischen den Optionen
 
     # Ergebnis: Vier Ausgabedateien, eine für jede Hypothese.
-    println(@sprintf("Simulated %i samples in %i iterations of gamble %i, participant %i.", gamble.numberOfSamples, PREDICTIONS_PER_GAMBLE, gamble.trigger, gamble.subject))
+    println(@sprintf("Thread %i: Simulated %i samples in %i iterations of gamble %i, participant %i.", Threads.threadid(), gamble.numberOfSamples, PREDICTIONS_PER_GAMBLE, gamble.trigger, gamble.subject))
 end
 
-rm(HYPOTHESIS_3_OUTPUT_FILE_NAME, force=true);
-CSV.write(HYPOTHESIS_3_OUTPUT_FILE_NAME, hypothesis3Data);
+for i in [3, 5, 6, 7]
+    rm(@sprintf("data/Study %i/hypothesis_%i_aggregated_data.csv", STUDY, i), force=true);
+end
+CSV.write(@sprintf("data/Study %i/hypothesis_%i_aggregated_data.csv", STUDY, 3), hypothesis3Data);
+CSV.write(@sprintf("data/Study %i/hypothesis_%i_aggregated_data.csv", STUDY, 5), hypothesis5Data);
 
 println("Run finished.")
--- generation_util.jl
+++ generation_util.jl
@@ -13,11 +13,11 @@ module generation_util
     const LOGGING_OUTPUT_PATH::String = "simulation.log";
 
     newSamplingPath = function(initialSize::Int64)
-        return (subject = Array{Int64}(undef, initialSize),     # participant
-                gamble = Array{Int64}(undef, initialSize),      # trigger
-                path = Array{Int64}(undef, initialSize),        # path = sequence of sampling instances
-                sample = Array{Int64}(undef, initialSize),      # index of a sampling instance in a path
-                AOI = Array{String}(undef, initialSize));
+        return (Array{Int64}(undef, initialSize),   # subject
+                Array{Int64}(undef, initialSize),   # trigger
+                Array{Int64}(undef, initialSize),   # path number
+                Array{Int64}(undef, initialSize),   # sample number within current path
+                Array{String}(undef, initialSize)); # AOI
     end
 
     newHypothesis3Data = function(initialSize::Int64)
@@ -31,8 +31,8 @@ module generation_util
 
     newHypothesis5Data = function(initialSize::Int64)
         keys = vcat([:subject, :gamble, :optionChosen], [Symbol(string(i)*"%") for i in 10:10:100]);
-        values = [fill([Array{Int64}(undef, initialSize)], 2)..., Array{Char}(undef, initialSize), fill([Array{Union{Float64, Missing}}(undef, initialSize)], 10)...];
-        return namedtuple(keys, values);
+        values = [Array{Int64}(undef, initialSize), Array{Int64}(undef, initialSize), Array{Char}(undef, initialSize), [Array{Union{Float64, Missing}}(undef, initialSize) for i in 1:10]...];
+        return (namedtuple(keys, values));
     end
 
     simulate = function(transitionMatrix::NamedMatrix{Float64, Matrix{Float64}, Tuple{OrderedDict{String, Int64}, OrderedDict{String, Int64}}},
@@ -40,82 +40,98 @@ module generation_util
                         gamble::DataFrameRow{DataFrame, DataFrames.Index},
                         iterations::Int64)
 
-        samplingPaths::NamedTuple{(:subject, :gamble, :path, :sample, :AOI), Tuple{Vector{Int64}, Vector{Int64}, Vector{Int64}, Vector{Int64}, Vector{String}}} = generation_util.newSamplingPath(gamble.numberOfSamples * iterations);
+        samplingPaths::Tuple{Array{Int64}, Array{Int64}, Array{Int64}, Array{Int64}, Array{String}} = generation_util.newSamplingPath(gamble.numberOfSamples * iterations);
         currentState::String = "";
 
         for i in 0:iterations-1
             currentState = StatsBase.sample(transition_matrix_creator.TARGETS, Weights(transition_matrix_creator.get_starting_point_probabilities(betas[1], betas[2], betas[3])), 1)[1];
-            map(x -> samplingPaths[x][1+i*gamble.numberOfSamples] = (gamble.subject, gamble.trigger, i+1, 1+i*gamble.numberOfSamples, currentState)[x], 1:5);
-    
+            foreach(x -> samplingPaths[x][1+i*gamble.numberOfSamples] = (gamble.subject, gamble.trigger, i+1, 1+i*gamble.numberOfSamples, currentState)[x], 1:5);
+
             for sample::Int64 in 2:gamble.numberOfSamples
                 currentState = StatsBase.sample(transition_matrix_creator.TARGETS, Weights(vec(transitionMatrix[currentState,:])), 1)[1];
-                map(x -> samplingPaths[x][sample+i*gamble.numberOfSamples] = (gamble.subject, gamble.trigger, i+1, sample+i*gamble.numberOfSamples, currentState)[x], 1:5);
+                foreach(x -> samplingPaths[x][sample+i*gamble.numberOfSamples] = (gamble.subject, gamble.trigger, i+1, sample+i*gamble.numberOfSamples, currentState)[x], 1:5);
             end
         end
-
-        return samplingPaths;
+        return DataFrame(subject = samplingPaths[1], gamble = samplingPaths[2], path = samplingPaths[3], sample = samplingPaths[4], AOI = samplingPaths[5], copycols=false);
     end
 
     """ Hypothesis 3: The amount of fixations spent on an outcome increases with both is probability and the value of the outcome.
     # Arguments
-    - samplingPaths: An array of sampling sequences of a single participant in a single gamble
+    - samplingPaths: A DataFrame of sampling sequences of a single participant in a single gamble
     # Returns
     - A NamedTuple containing the average percentage of fixations on an outcome across all sampling sequences
     """
-    calculateHypothesis3 = function(samplingPaths::NamedTuple{(:subject, :gamble, :path, :sample, :AOI), Tuple{Vector{Int64}, Vector{Int64}, Vector{Int64}, Vector{Int64}, Vector{String}}})
-        numAOIs::Dict{String, Int64} = countmap(samplingPaths[:AOI]);
-        numTotalSamples::Int64 = length(samplingPaths[1]);
+    calculateHypothesis3 = function(samplingPaths::DataFrame)
+        numAOIs::Dict{String, Int64} = countmap(samplingPaths[:, :AOI]);
+        numTotalSamples::Int64 = size(samplingPaths)[1];
 
-        return (subject = samplingPaths.subject[1],
-                gamble = samplingPaths.gamble[1],
+        return DataFrame(subject = samplingPaths[1, :subject],
+                gamble = samplingPaths[1, :gamble],
                 Av1 = (haskey(numAOIs, "Av1") ? numAOIs["Av1"] : 0) / numTotalSamples,
                 Av2 = (haskey(numAOIs, "Av2") ? numAOIs["Av2"] : 0) / numTotalSamples,
                 Bv1 = (haskey(numAOIs, "Bv1") ? numAOIs["Bv1"] : 0) / numTotalSamples,
-                Bv2 = (haskey(numAOIs, "Bv2") ? numAOIs["Bv2"] : 0) / numTotalSamples);
+                Bv2 = (haskey(numAOIs, "Bv2") ? numAOIs["Bv2"] : 0) / numTotalSamples)[1, :];
     end
 
     """ Hypothesis 5: In the last third of a sampling process, there's a bias towards the ultimately chosen option (gaze-cascade effect).
     # Arguments
-    - samplingPaths: An array of sampling sequences of a single participant in a single gamble
+    - samplingPaths: A DataFrame of one or more sampling sequences of a single participant in a single gamble
+    - optionChosen: The option the participant has ultimately chosen, either 'A' or 'B'
     # Returns
+    - A NamedTuple containing the percentage of fixations of the ultimately chosen option in ten-percent steps of the total sampling duration
     """
-    calculateHypothesis5 = function(samplingPaths::NamedTuple{(:subject, :gamble, :path, :sample, :AOI), Tuple{Vector{Int64}, Vector{Int64}, Vector{Int64}, Vector{Int64}, Vector{String}}},
-                                    optionChosen::Char)
+    calculateHypothesis5 = function(samplingPaths::DataFrame, optionChosen::Char)
+
         calculateTargetOptionRatio = function(pathSegment::Array{String}, optionChosenTargets::Array{String})
             numAOIs::Dict{String, Int64} = countmap(pathSegment);
             return sum(map(target -> numAOIs[target], intersect(optionChosenTargets, OrderedCollections.keys(numAOIs)))) / length(pathSegment);
         end
+
         logger::FormatLogger = FormatLogger(open(LOGGING_OUTPUT_PATH, "w")) do io, args
             println(io, args.message);
         end
-        samplingPathLength::Int64 = size(samplingPaths)[1];
+
+        samplingPathNumbers::Array{Int64} = unique(samplingPaths[:, :path]);
+        samplingPathLength::Int64 = size(samplingPaths)[1] / size(samplingPathNumbers)[1];
         percentages::Array{Symbol} = [Symbol(string(i)*"%") for i in 10:10:100];
-        keys::NTuple{13, Symbol} = Tuple(vcat([:subject, :gamble, :optionChosen], percentages));
-        values::NTuple{13, Union{String, Missing}} = Tuple([samplingPaths[1,:subject], samplingPaths[1,:gamble], optionChosen, fill(missing, 10)...]);
-        optionChosenPercentages = namedtuple(keys, values);
+        averageTargetSamplingRatio::DataFrame = DataFrame(
+            subject = Array{Int64}([samplingPaths[1, :subject]]),
+            gamble = Array{Int64}([samplingPaths[1, :gamble]]),
+            optionChosen = Array{Char}([optionChosen])
+        );
+        foreach(column -> (averageTargetSamplingRatio[:, column] = Array{Union{Float64, Missing}}(missing, 1)), percentages);
 
         if(samplingPathLength < 3)
             with_logger(logger) do 
                 @info @sprintf("Subject %i showed less than three samples in gamble %i and will be excluded from testing hypothesis 5.", samplingPaths[1,:subject], samplingPaths[1, :gamble]);
             end
-            return optionChosenPercentages;
+            return averageTargetSamplingRatio[1, :];
         end
         
+        currentSamplingPath::Array{String} = [];
         numOfSegments::Int64 = 10;
         avgSegmentSize::Rational{Int64} = samplingPathLength // numOfSegments;
-        pathSegments::Array{Array{String}} = [samplingPath[round(Int64, i*avgSegmentSize)+1:(i === numOfSegments-1 ? end : round(Int64, ((i+1)*avgSegmentSize)))] for i in 0:numOfSegments-1];
-        optionChosenTargets::Array{String} = optionChosen === 'A' ? ["Ap1", "Ap2", "Av1", "Av2"]
-                                           : optionChosen === 'B' ? ["Bp1", "Bp2", "Bv1", "Bv2"]
-                                           : error(@sprintf("Expected 'A' or 'B' for option chosen, got '%s' instead.", optionChosen));
-        # Achtung, hier kracht es, wenn ein PathSegment eingegeben wird, das weniger als 10 Elemente hat.
-        targetOptionRatios::NTuple{10, Union{Float64, Missing}} = namedtuple(percentages, map(segment -> calculateTargetOptionRatio(segment, optionChosenTargets), pathSegments));
-        foreach(percentage -> optionChosenPercentages[percentage] = targetOptionRatios[percentage], percentages);
+        targetSamplingRatio = Matrix{Union{Float64, Missing}}(missing, size(samplingPathNumbers)[1], numOfSegments);
+        pathSegments::Array{Array{String}} = [];
+        optionChosenTargets::Array{String} = [];
 
-        # kumuliert: Schritt n beinhaltet alle Fixationen von 0 bis Schritt n.
-        # nicht-kumuliert: Schritt n beinhaltet alle Fixationen von Schritt n-1 bis Schritt n.
+        for samplingPathNumber::Int64 in samplingPathNumbers
+            currentSamplingPath = samplingPaths[samplingPaths[!, :path] .=== samplingPathNumber, :][!, :AOI];
+            pathSegments = [currentSamplingPath[round(Int64, i*avgSegmentSize)+1:(i === numOfSegments-1 ? end : round(Int64, ((i+1)*avgSegmentSize)))] for i in 0:numOfSegments-1];
+            optionChosenTargets = optionChosen === 'A' ? ["Ap1", "Ap2", "Av1", "Av2"] :
+                                  optionChosen === 'B' ? ["Bp1", "Bp2", "Bv1", "Bv2"] :
+                                  error(@sprintf("Expected 'A' or 'B' for option chosen, got '%s' instead.", optionChosen));
+            
+            for i::Int64 in eachindex(pathSegments)
+                if(!isempty(pathSegments[i]))
+                    targetSamplingRatio[samplingPathNumber, i] = calculateTargetOptionRatio(pathSegments[i], optionChosenTargets);
+                end
+            end
+        end
 
-        # Wenn der Pfad weniger als 10 Samples lang ist, dann wird die Position jedes Wertes im Zeitablauf anteilig berechnet und gerundet.
-
-        # Die Antwort auf diese Hypothese wird mglw. durch den grundsätzlichen Effekt verfälscht, dass Teilnehmer ca. im ersten Drittel bevorzugt Optionen auf der linken Seite anschauen.
+        for i::Int64 in 1:numOfSegments
+            averageTargetSamplingRatio[1, percentages[i]] = ismissing(targetSamplingRatio[1, i]) ? missing : mean(targetSamplingRatio[:, i]);
+        end
+        return averageTargetSamplingRatio[1, :];
     end
 end
