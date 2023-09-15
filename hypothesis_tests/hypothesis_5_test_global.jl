b54ac10a73ddf76890a07ac0a71185034361c310
unknown <Oliver.Kurtz@studium.fernuni-hagen.de>
2023-09-15 23:41:02 +0200

    fix erroneous group calculation

--- hypothesis_tests/hypothesis_5_test_global.jl
+++ hypothesis_tests/hypothesis_5_test_global.jl
@@ -12,21 +12,9 @@ meanskipmissing = function(itr)
     return mean(skipmissing(itr));
 end
 data = @pipe CSV.read("data/Study 1/hypothesis_5_aggregated_data.csv", DataFrame) |> filter(row -> !(all(ismissing, row[4:13])), _);
-groupA = @pipe data[data[!, :optionChosen] .== "A", :] |>
-    select!(_, Not([:gamble, :optionChosen])) |>
-    groupby(_, :subject) |>
-    combine(_, Symbol("10%") => mean, Symbol("20%") => meanskipmissing, Symbol("30%") => meanskipmissing, Symbol("40%") => meanskipmissing,
-        Symbol("50%") => meanskipmissing, Symbol("60%") => meanskipmissing, Symbol("70%") => meanskipmissing, Symbol("80%") => meanskipmissing,
-        Symbol("90%") => meanskipmissing, Symbol("100%") => meanskipmissing, renamecols=false);
-groupB = @pipe data[data[!, :optionChosen] .== "B", :] |>
-    select!(_, Not([:gamble, :optionChosen])) |>
-    groupby(_, :subject) |>
-    combine(_, Symbol("10%") => mean, Symbol("20%") => meanskipmissing, Symbol("30%") => meanskipmissing, Symbol("40%") => meanskipmissing,
-        Symbol("50%") => meanskipmissing, Symbol("60%") => meanskipmissing, Symbol("70%") => meanskipmissing, Symbol("80%") => meanskipmissing,
-        Symbol("90%") => meanskipmissing, Symbol("100%") => meanskipmissing, renamecols=false);
-groupA = map(row -> mean(skipmissing(Array(row[2:11]))), eachrow(groupA));
-groupB = map(row -> mean(skipmissing(Array(row[2:11]))), eachrow(groupB));
+groupA = map(col -> mean(skipmissing(col)), eachcol(data[data[!, :optionChosen] .== "A", :][!, 4:13]));
+groupB = map(col -> mean(skipmissing(col)), eachcol(data[data[!, :optionChosen] .== "B", :][!, 4:13]));
 t_test = OneSampleTTest(groupA, 0.5);   # Oder groupB, 0.5
-pvalue(t_test, tail = :left)
+pvalue(t_test, tail = :right)
 confint(t_test)
 d = EffectSizes.CohenD(groupA, groupB)
