cac7b9b98c3b850930beade82e01a8a9c915ac8b
unknown <Oliver.Kurtz@studium.fernuni-hagen.de>
2023-09-16 23:39:51 +0200

    add box plot for hypothesis 7

---
+++ plots/hypothesis_7.jl
@@ -0,0 +1,47 @@
+using
+    CSV,
+    DataFrames,
+    Pipe,
+    PlotlyJS;
+
+cd("C:\\Users\\Oliver\\Documents\\Studium\\Psychologie\\Bachelorarbeit\\Skripte");
+
+study1Data = @pipe CSV.read("data/Study 1/hypothesis_7_aggregated_data.csv", DataFrame, select=[:fixWithinLottery]);
+study2Data = @pipe CSV.read("data/Study 2/hypothesis_7_aggregated_data.csv", DataFrame, select=[:fixWithinLottery]);
+study1Data.study .= "Studie 1";
+study2Data.study .= "Studie 2";
+plotData = append!(study1Data, study2Data)
+
+boxplot = plot(
+    plotData,
+    x = :study,
+    y = :fixWithinLottery,
+    kind="box",
+    labels = Dict(
+        :study => "",
+        :fixWithinLottery => "Anteil Fixationsübergänge innerhalb einer Lotterie"
+    ),
+    Layout(
+        title = attr(
+            text = "Verteilung der Fixationsübergänge innerhalb einer Lotterie",
+            x = 0.5,
+            xanchor = "center"
+        ),
+        plot_bgcolor = "white",
+        colorway = ["black"],
+        yaxis = attr(
+            showgrid = true,
+            zeroline = true,
+            showline = true,
+            gridcolor = "#bdbdbd",
+            gridwidth = 2,
+            zerolinecolor = "#969696",
+            zerolinewidth = 2,
+            linecolor = "#636363",
+            linewidth = 2,
+            tickmode = "linear",
+            tick0 = 0.65,
+            dtick = 0.05
+        )
+    )
+);
